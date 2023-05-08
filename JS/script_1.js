5565e8a522298ffdadee9636d149233b3a6209ec
Thales Lourençon <109592406+thales4110@users.noreply.github.com>
2023-05-08 12:55:56 -0300

    project finished so far

--- JS/script_1.js
+++ JS/script_1.js
@@ -2,16 +2,23 @@ class Product {
     constructor() {
         this.id = 1
         this.productsArray = []
+        this.editId = null
     }
 
     save() {
        let product = this.readData()
 
        if(this.validateField(product)) {
-            this.add(product)
+            if(this.editId == null){
+                this.add(product)
+            } else {
+                this.update(this.editId, product)
+            }
+           
        }
 
        this.tableList()
+       this.cancel()
     }
 
     tableList() {
@@ -35,8 +42,11 @@ class Product {
 
             let imgEdit = document.createElement('img')
             imgEdit.src = '/img/edit.svg'
+            imgEdit.setAttribute("onclick","product.prepareEdition("+ JSON.stringify(this.productsArray[i]) + ")")
+
             let imgDelete = document.createElement('img')
             imgDelete.src = '/img/delete.svg'
+            imgDelete.setAttribute("onclick","product.delete("+ this.productsArray[i].id +")")
 
             td_action.appendChild(imgEdit)
             td_action.appendChild(imgDelete)
@@ -44,8 +54,28 @@ class Product {
     }
 
     add(product) {
+        product.productPrice = parseFloat(product.productPrice)
         this.productsArray.push(product)
         this.id ++
+    }
+
+    update(id, product) {
+        for(let i = 0; i < this.productsArray.length; i++){
+            if(this.productsArray[i].id == id) {
+                this.productsArray[i].productName = product.productName
+                this.productsArray[i].productPrice = product.productPrice
+            }
+        }
+    }
+
+    prepareEdition(data) {
+
+        this.editId = data.id
+
+        document.getElementById('product').value = data.productName
+        document.getElementById('price').value = data.productPrice
+
+        document.getElementById('btn1').innerText = 'Update'
     }
 
     readData() {
@@ -76,6 +106,32 @@ class Product {
 
         return true
     }
+
+    cancel() {
+        document.getElementById('product').value = ''
+        document.getElementById('price').value = ''
+
+        document.getElementById('btn1').innerText = 'Save'
+        this.editId = null
+    }
+
+    delete(id) {
+
+        if(confirm('confirm the deletion of the product id: ' + id)){
+            
+            let tbody =document.getElementById('tbody')
+
+            for(let i = 0; i < this.productsArray.length; i++) {
+                if(this.productsArray[i].id == id) {
+                    this.productsArray.splice(i, 1)
+                    tbody.deleteRow(i)
+                }
+            }
+        }
+       
+    }
+
+
 }
 
 var product = new Product()
